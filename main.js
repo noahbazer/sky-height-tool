41f8f0fa8a90fac7bc5f45f27e959b16a8e9ee46
Noah Bazer <125102673+noahbazer@users.noreply.github.com>
2024-07-24 22:19:20 -0400

    new visualizer, new stats

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ const screens = {
                 </div>
                 <div class="slider-container">
                     <div class="horizontal-line"></div>
-                    <input orient="vertical" type="range" min="10" max="500" value="255" class="slider vertical" oninput="updateLinePosition(event);">
+                    <input orient="vertical" type="range" min="10" max="500" value="255" class="slider vertical" id="verticalsliderinput" oninput="updateLinePosition(event);">
                 </div>
             </div>
             <div class="tool-controls">
@@ -54,9 +54,20 @@ const screens = {
         </div>
         <div class="results">
             <h3>Results</h3>
-            <p class="results-text">Your height is <span class="height">-</span> of the natural maximum!</p>
-            <strong>What does this mean?</strong>
-            <p>Out of the 4000 heights naturally available (without using temporary items or the chibi mask), your height is greater than <span class="height">-</span> of them.</p>
+            <div class="slider-results-container">
+              <img src="./assets/slidericons.png" class="icon1">
+              <div class="slider-results">
+                <div class="slider-thumb" id="sliderthumb">
+                  <p>YOU</p>
+                  <div class="triangle"></div>
+                </div>
+                <div class="slider-line"></div>
+              </div>
+              <img src="./assets/slidericons.png" class="icon2">
+            </div>
+            <div class="results-stat">
+              <p class="results-text">Your height is <span class="height">50%</span> of the natural maximum!</p>
+            </div>
             <h2>Things to consider:</h2>
             <p>1. The tool is driven by data from other users. You can improve it's accuracy by posting your results <br><a class="dislink2" href="https://discord.gg/Qy7V6K2auw">In our discord!</a></p>
             <p>2. There is a rumor that each player has a unique height modifier that affects their height. This tool does not (and cannot) account for that.</p>
@@ -67,23 +78,23 @@ const screens = {
 
 const steps = [
   {
-    title: "Step 1:",
-    image: "assets/step1.png",
-    text: "Go inside the hair shop in Aviary village.",
+    title: 'Step 1:',
+    image: 'assets/step1.png',
+    text: 'Go inside the hair shop in Aviary village.',
   },
   {
-    title: "Step 2",
-    image: "assets/step2.png",
+    title: 'Step 2',
+    image: 'assets/step2.png',
     text: `Once inside, go sit down on the MIDDLE chair.`,
   },
   {
-    title: "Step 3",
-    image: "assets/step3.png",
-    text: "Select the default hair.",
+    title: 'Step 3',
+    image: 'assets/step3.png',
+    text: 'Select the default hair.',
   },
   {
-    title: "Step 4",
-    image: "assets/step4.png",
+    title: 'Step 4',
+    image: 'assets/step4.png',
     text: "Take a screenshot with the hair menu open. Don't worry if the chair swivels, this won't affect your results.",
   },
 ];
@@ -94,27 +105,27 @@ steps.forEach((step) => {
 });
 
 const goToUploadHandler = () => {
-  frame.style.transition = "opacity 1s";
+  frame.style.transition = 'opacity 1s';
   frame.style.opacity = 0;
   setTimeout(() => {
     frame.innerHTML = screens.tool;
     frame.style.opacity = 1;
-    const horizontalSlider = document.querySelector(".slider-zoom");
-    const header = document.querySelector(".header");
+    const horizontalSlider = document.querySelector('.slider-zoom');
+    const header = document.querySelector('.header');
     console.log(header);
 
-    console.log("Script started");
-    window.addEventListener("scroll", function () {
+    console.log('Script started');
+    window.addEventListener('scroll', function () {
       if (window.scrollY > 0) {
-        console.log("Adding scrolled class");
-        header.classList.add("scrolled");
+        console.log('Adding scrolled class');
+        header.classList.add('scrolled');
       } else {
-        console.log("Removing scrolled class");
-        header.classList.remove("scrolled");
+        console.log('Removing scrolled class');
+        header.classList.remove('scrolled');
       }
     });
 
-    horizontalSlider.addEventListener("dragstart", (event) => {
+    horizontalSlider.addEventListener('dragstart', (event) => {
       event.preventDefault();
     });
     updateLinePosition();
@@ -123,12 +134,12 @@ const goToUploadHandler = () => {
     function handleFileUpload(event) {
       const file = event.target.files[0];
       const url = URL.createObjectURL(file);
-      const uploadImage = document.querySelector(".upload-image");
+      const uploadImage = document.querySelector('.upload-image');
 
       if (uploadImage) {
         uploadImage.src = url;
 
-        resizeUploadImage({ target: document.querySelector(".slider-zoom") });
+        resizeUploadImage({ target: document.querySelector('.slider-zoom') });
 
         let isDragging = false;
         let startX, startY, initialOffsetX, initialOffsetY;
@@ -146,8 +157,8 @@ const goToUploadHandler = () => {
           event.preventDefault();
           const dx = (event.clientX || event.touches[0].clientX) - startX;
           const dy = (event.clientY || event.touches[0].clientY) - startY;
-          uploadImage.style.left = initialOffsetX + dx + "px";
-          uploadImage.style.top = initialOffsetY + dy + "px";
+          uploadImage.style.left = initialOffsetX + dx + 'px';
+          uploadImage.style.top = initialOffsetY + dy + 'px';
         }
 
         function stopDragging() {
@@ -155,7 +166,7 @@ const goToUploadHandler = () => {
         }
 
         document.addEventListener(
-          "touchmove",
+          'touchmove',
           function (event) {
             if (isDragging) {
               event.preventDefault();
@@ -164,25 +175,25 @@ const goToUploadHandler = () => {
           { passive: false }
         );
 
-        uploadImage.addEventListener("mousedown", startDragging);
-        uploadImage.addEventListener("touchstart", startDragging);
+        uploadImage.addEventListener('mousedown', startDragging);
+        uploadImage.addEventListener('touchstart', startDragging);
 
-        window.addEventListener("mousemove", drag);
-        window.addEventListener("touchmove", drag);
+        window.addEventListener('mousemove', drag);
+        window.addEventListener('touchmove', drag);
 
-        window.addEventListener("mouseup", stopDragging);
-        window.addEventListener("touchend", stopDragging);
+        window.addEventListener('mouseup', stopDragging);
+        window.addEventListener('touchend', stopDragging);
 
-        uploadImage.addEventListener("dragstart", (event) => {
+        uploadImage.addEventListener('dragstart', (event) => {
           event.preventDefault();
         });
       }
     }
 
     // Attach the handleFileUpload function to the file input element
-    const fileInput = document.querySelector("#file-input");
+    const fileInput = document.querySelector('#file-input');
     if (fileInput) {
-      fileInput.addEventListener("change", handleFileUpload);
+      fileInput.addEventListener('change', handleFileUpload);
     }
   }, 1000);
 };
@@ -190,34 +201,34 @@ const goToUploadHandler = () => {
 let step = 0;
 
 const handleStep = () => {
-  const stepImage = document.querySelector(".step-image");
-  const stepTitle = document.querySelector(".step-title");
-  const stepText = document.querySelector(".step-text");
+  const stepImage = document.querySelector('.step-image');
+  const stepTitle = document.querySelector('.step-title');
+  const stepText = document.querySelector('.step-text');
   stepImage.src = steps[step].image;
   stepTitle.textContent = steps[step].title;
   stepText.innerHTML = steps[step].text;
   if (step === 0) {
-    const stepButtonBack = document.querySelector(".step-button-back");
-    stepButtonBack.style.display = "none";
+    const stepButtonBack = document.querySelector('.step-button-back');
+    stepButtonBack.style.display = 'none';
   } else {
-    const stepButtonBack = document.querySelector(".step-button-back");
-    stepButtonBack.style.display = "flex";
+    const stepButtonBack = document.querySelector('.step-button-back');
+    stepButtonBack.style.display = 'flex';
   }
   if (step === 3) {
-    const stepButtonNext = document.querySelector(".step-button-next");
+    const stepButtonNext = document.querySelector('.step-button-next');
     stepButtonNext.innerHTML =
       'Go to Upload<span class="fa-solid fa-arrow-right upload-step"></span>';
-    stepButtonNext.addEventListener("click", goToUploadHandler);
+    stepButtonNext.addEventListener('click', goToUploadHandler);
   } else {
-    const stepButtonNext = document.querySelector(".step-button-next");
-    stepButtonNext.innerHTML = "Next";
-    stepButtonNext.removeEventListener("click", goToUploadHandler);
+    const stepButtonNext = document.querySelector('.step-button-next');
+    stepButtonNext.innerHTML = 'Next';
+    stepButtonNext.removeEventListener('click', goToUploadHandler);
   }
 };
 
 const handleBackButton = () => {
-  const stepButtonBack = document.querySelector(".step-button-back");
-  stepButtonBack.addEventListener("click", () => {
+  const stepButtonBack = document.querySelector('.step-button-back');
+  stepButtonBack.addEventListener('click', () => {
     if (step === 0) {
       return;
     }
@@ -227,8 +238,8 @@ const handleBackButton = () => {
 };
 
 const handleNextButton = () => {
-  const stepButtonNext = document.querySelector(".step-button-next");
-  stepButtonNext.addEventListener("click", () => {
+  const stepButtonNext = document.querySelector('.step-button-next');
+  stepButtonNext.addEventListener('click', () => {
     if (step === 3) {
       return;
     }
@@ -244,39 +255,39 @@ const beginStepSequence = () => {
   }, 500);
 };
 
-const startButton = document.querySelector(".button-begin");
-const frame = document.querySelector(".frame");
+const startButton = document.querySelector('.button-begin');
+const frame = document.querySelector('.frame');
 
 //make the intro screen fade away before the route screen appears
 const startButtonHandler = () => {
-  const intro = document.querySelector(".intro");
-  intro.style.transition = "opacity 1s";
+  const intro = document.querySelector('.intro');
+  intro.style.transition = 'opacity 1s';
   intro.style.opacity = 0;
-  intro.style.pointerEvents = "none";
+  intro.style.pointerEvents = 'none';
   setTimeout(() => {
     frame.innerHTML = screens.route;
-    const stepsButton = document.querySelector(".steps-link");
+    const stepsButton = document.querySelector('.steps-link');
     const stepsButtonHandler = () => {
-      const route = document.querySelector(".route");
-      route.style.transition = "opacity .5s";
+      const route = document.querySelector('.route');
+      route.style.transition = 'opacity .5s';
       route.style.opacity = 0;
-      route.style.pointerEvents = "none";
+      route.style.pointerEvents = 'none';
       setTimeout(() => {
         frame.innerHTML = screens.steps;
       }, 500);
       beginStepSequence();
     };
     if (stepsButton) {
-      stepsButton.addEventListener("click", stepsButtonHandler);
+      stepsButton.addEventListener('click', stepsButtonHandler);
     }
   }, 1000);
 };
 
-startButton.addEventListener("click", startButtonHandler);
+startButton.addEventListener('click', startButtonHandler);
 
 function updateLinePosition() {
-  const verticalSlider = document.querySelector(".slider.vertical");
-  const horizontalLine = document.querySelector(".horizontal-line");
+  const verticalSlider = document.querySelector('.slider.vertical');
+  const horizontalLine = document.querySelector('.horizontal-line');
   const sliderMin = parseInt(verticalSlider.min);
   const sliderMax = parseInt(verticalSlider.max);
   const sliderValue = parseInt(verticalSlider.value);
@@ -291,39 +302,47 @@ function updateLinePosition() {
 
   horizontalLine.style.top = `${topPosition}px`;
   if (sliderPercentage === 100 || sliderPercentage === 0) {
-    const resultsText = document.querySelector(".results-text");
+    const resultsText = document.querySelector('.results-text');
     resultsText.innerHTML = `Your height is <span style="color: #32CD32;">${
       sliderPercentage === 100
-        ? "close to the natural maximum!"
-        : "close to the natural minimum!"
+        ? 'close to the natural maximum!'
+        : 'close to the natural minimum!'
     }</span>`;
   } else {
-    const resultsText = document.querySelector(".results-text");
+    const resultsText = document.querySelector('.results-text');
     resultsText.innerHTML = `Your height is <span class="height">${sliderPercentage.toFixed(
       2
     )}</span> of the natural maximum!`;
   }
 
-  const heightElements = document.querySelectorAll(".height");
+  const sliderThumb = document.querySelector('.slider-thumb');
+  // Calculate the position percentage
+  const percentage =
+    ((sliderValue - sliderMin) / (sliderMax - sliderMin)) * 100;
+
+  // Update the marker group position
+  sliderThumb.style.left = `calc(${percentage}% - 17px)`;
+
+  const heightElements = document.querySelectorAll('.height');
   heightElements.forEach((element) => {
     if (sliderPercentage <= 10) {
-      element.style.color = "#32CD32"; // Lime green for values 0-10
+      element.style.color = '#32CD32'; // Lime green for values 0-10
     } else if (sliderPercentage <= 20) {
-      element.style.color = "#9ACD32"; // Yellow green for values 20-30
+      element.style.color = '#9ACD32'; // Yellow green for values 20-30
     } else if (sliderPercentage <= 30) {
-      element.style.color = "#FFFF00"; // Yellow for values 10-20
+      element.style.color = '#FFFF00'; // Yellow for values 10-20
     } else if (sliderPercentage <= 40) {
-      element.style.color = "#FFA500"; // Orange for values 30-40
+      element.style.color = '#FFA500'; // Orange for values 30-40
     } else if (sliderPercentage <= 50) {
-      element.style.color = "#FF0000"; // Red for values 40-50
+      element.style.color = '#FF0000'; // Red for values 40-50
     } else if (sliderPercentage <= 60) {
-      element.style.color = "#FFA500"; // Orange for values 50-60
+      element.style.color = '#FFA500'; // Orange for values 50-60
     } else if (sliderPercentage <= 70) {
-      element.style.color = "#FFFF00"; // Yellow for values 60-70
+      element.style.color = '#FFFF00'; // Yellow for values 60-70
     } else if (sliderPercentage <= 80) {
-      element.style.color = "#9ACD32"; // Yellow green for values 70-80
+      element.style.color = '#9ACD32'; // Yellow green for values 70-80
     } else {
-      element.style.color = "#32CD32"; // Lime green for values 90-100
+      element.style.color = '#32CD32'; // Lime green for values 90-100
     }
     element.textContent = `${sliderPercentage.toFixed(2)}%`;
   });
@@ -332,22 +351,22 @@ function updateLinePosition() {
 function handleFileUpload(event) {
   const file = event.target.files[0];
   const url = URL.createObjectURL(file);
-  const uploadImage = document.querySelector(".upload-image");
+  const uploadImage = document.querySelector('.upload-image');
   uploadImage.src = url;
 }
 
 function resizeUploadImage() {
-  const horizontalSlider = document.querySelector(".slider-zoom");
+  const horizontalSlider = document.querySelector('.slider-zoom');
   const size = horizontalSlider.value / 100;
-  const uploadImage = document.querySelector(".upload-image");
+  const uploadImage = document.querySelector('.upload-image');
   uploadImage.style.transform = `scale(${size})`;
 }
 
 const handleRadioChange = (angle) => {
-  const templateImage = document.querySelector(".template-image");
-  if (angle === "a") {
-    templateImage.src = "assets/lanterntemplate.png";
+  const templateImage = document.querySelector('.template-image');
+  if (angle === 'a') {
+    templateImage.src = 'assets/lanterntemplate.png';
   } else {
-    templateImage.src = "assets/lanterntemplateb.png";
+    templateImage.src = 'assets/lanterntemplateb.png';
   }
 };
